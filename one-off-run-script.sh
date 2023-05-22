ba87ba5c97ff4e828a6cbfa363d3e955e2e61b4b
Miguel Lopez <66633291+mg-lopez@users.noreply.github.com>
2023-05-22 17:11:55 +0100

    added one-off-script.sh v1.0

--- one-off-run-script.sh
+++ one-off-run-script.sh
@@ -3,6 +3,9 @@
 echo 'Ensuring SELinux is installed:'
 sudo yum install selinux-policy-devel
 
+echo 'Ensuring docker is running'
+sudo systemctl start docker
+
 echo 'Ensuring that docker has SELinux enabled:'
 sudo sed -i 's|ExecStart=/usr/bin/dockerd|ExecStart=/usr/bin/dockerd --selinux-enabled|' /usr/lib/systemd/system/docker.service
 
@@ -13,12 +16,12 @@ echo 'confirm docker is running with SELinux enabled:'
 docker info | grep -A5 Security
 
 echo 'Compiling the restrive .te SELinux Policy for the Database:'
-sudo make -f /usr/share/selinux/devel/Makefile ~/iss2023-lima/iss2023-lima-db_c.pp
+sudo make -f /usr/share/selinux/devel/Makefile ~/iss2023-lima/dbserver/iss2023-lima-db_c.pp
 sudo semodule -i ~/iss2023-lima/iss2023-lima-db_c.pp
 
 
 echo 'Compiling the restrive .te SELinux Policy for the Database:'
-sudo make -f /usr/share/selinux/devel/Makefile ~/iss2023-lima/iss2023-lima-web_c.pp
+sudo make -f /usr/share/selinux/devel/Makefile ~/iss2023-lima/web/server/iss2023-lima-web_c.pp
 sudo semodule -i ~/iss2023-lima/iss2023-lima-web_c.pp
 
 echo 'Ensure that SELinux is enforcing:'
