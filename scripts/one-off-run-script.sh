e68e4c345b7f87232c6e0fd149c0a6a1d6f9c707
Miguel Lopez <66633291+mg-lopez@users.noreply.github.com>
2023-05-23 22:08:17 +0100

    Update one-off-run-script.sh

--- scripts/one-off-run-script.sh
+++ scripts/one-off-run-script.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
-# Ensuring docker is running
-sudo systemctl start docker
-
 echo 'Ensuring SELinux is installed:'
 sudo yum install selinux-policy-devel
+
+echo 'Ensuring docker is running'
+sudo systemctl start docker
 
 echo 'Ensuring that docker has SELinux enabled:'
 sudo sed -i 's|ExecStart=/usr/bin/dockerd|ExecStart=/usr/bin/dockerd --selinux-enabled|' /usr/lib/systemd/system/docker.service
 
 echo 'Restarting docker...'
-sudo systemctl daemon-reload
+sudo systemctl daemon-reaload
 
 echo 'confirm docker is running with SELinux enabled:'
 docker info | grep -A5 Security
 
-cd /home/csc/iss2023-lima/builds/dbserver
-
 echo 'Compiling the restrive .te SELinux Policy for the Database:'
+cd /home/csc/iss2023-lima/builds/dbserver
 sudo make -f /usr/share/selinux/devel/Makefile iss2023-lima-db_c.pp
+cd /home/csc/iss2023-lima/builds/dbserver
 sudo semodule -i iss2023-lima-db_c.pp
 
-cd /home/csc/iss2023-lima/builds/webserver
 
 echo 'Compiling the restrive .te SELinux Policy for the Database:'
+cd /home/csc/iss2023-lima/builds/webserver
 sudo make -f /usr/share/selinux/devel/Makefile iss2023-lima-web_c.pp
+cd /home/csc/iss2023-lima/builds/webserver
 sudo semodule -i iss2023-lima-web_c.pp
 
 echo 'Ensure that SELinux is enforcing:'
