7ed7dec7e0f88c1e3090fa3bbf5dee171f387b4e
LouisStaff <90843951+LouisStaff@users.noreply.github.com>
2023-05-24 20:32:29 +0100

    Update buildscript.sh

--- scripts/buildscript.sh
+++ scripts/buildscript.sh
@@ -12,16 +12,8 @@ cd /home/csc/iss2023-lima/builds/dbserver
 # Builds database image from Dockerfile
 sudo docker build -t iss2023/lima-db_i .
 
-chmod +x strip-cmd
-chmod +x strip-image
-sudo ./strip-cmd
-
 # Change directory to 'webserver'
 cd /home/csc/iss2023-lima/builds/webserver
 
 # Builds webserver image from Dockerfile
 sudo docker build -t iss2023/lima-web_i .
-
-chmod +x strip-cmd
-chmod +x strip-image
-sudo ./strip-cmd
