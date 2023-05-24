ebc815d0527f166388951fd7b901165a96a90250
LouisStaff <90843951+LouisStaff@users.noreply.github.com>
2023-05-24 17:16:54 +0100

    Update repeated-run-script.sh

--- scripts/repeated-run-script.sh
+++ scripts/repeated-run-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Ensuring docker is running
-sudo systemctl start docker
+sudo systemctl restart docker
 
 # Change directory to 'dbserver'
 cd /home/csc/iss2023-lima/builds/dbserver
@@ -20,4 +20,3 @@ cd /home/csc/iss2023-lima/builds/webserver
 
 # Runs the webserver container
 docker run -d --net iss2023/lima_n --ip 198.51.100.149 --hostname www.iss.cyber23.test --add-host db.iss.cyber23.test:198.51.100.150 -p 8080:80 --name iss2023-lima-web_c --security-opt seccomp=$PWD/docker_webserver.json --security-opt label:type:iss2023-lima-web_c_t iss2023/lima-web-stripped_i
-
