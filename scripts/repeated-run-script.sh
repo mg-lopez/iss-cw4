888e0750d3ee5a869f2fc4a97ec27d8d1f97b0b0
LouisStaff <90843951+LouisStaff@users.noreply.github.com>
2023-05-24 22:15:17 +0100

    Update repeated-run-script.sh

--- scripts/repeated-run-script.sh
+++ scripts/repeated-run-script.sh
@@ -7,7 +7,7 @@ sudo systemctl start docker
 cd /home/csc/iss2023-lima/builds/dbserver
 
 # Runs the database container
-docker run -d --net iss2023/lima_n --ip 198.51.100.150 --hostname db.iss.cyber23.test -e MYSQL_ROOT_PASSWORD="CorrectHorseBatteryStaple-xkcd" -e MYSQL_DATABASE="iss2023db" --name iss2023-lima-db_c --security-opt seccomp=$PWD/docker_dbserver.json --security-opt label:type::docker_dbserver_c_t --volume $(pwd)/mariadb:/var/lib/mariadb:Z iss2023/lima-db-stripped_i
+docker run -d --net iss2023/lima_n --ip 198.51.100.150 --hostname db.iss.cyber23.test -e MYSQL_ROOT_PASSWORD="CorrectHorseBatteryStaple-xkcd" -e MYSQL_DATABASE="iss2023db" --name iss2023-lima-db_c --security-opt seccomp=$PWD/docker_dbserver.json --security-opt label:type::docker_dbserver_c_t iss2023/lima-db-stripped_i
 
 # Wait for 10 seconds
 sleep 10
@@ -19,4 +19,4 @@ docker exec -i iss2023-lima-db_c mysql -uroot -pCorrectHorseBatteryStaple-xkcd <
 cd /home/csc/iss2023-lima/builds/webserver
 
 # Runs the webserver container
-docker run -d --net iss2023/lima_n --ip 198.51.100.149 --hostname www.iss.cyber23.test --add-host db.iss.cyber23.test:198.51.100.150 -p 8080:80 --name iss2023-lima-web_c --security-opt seccomp=$PWD/docker_webserver.json --security-opt label:type:docker_webserver_c_t --volume $(pwd)/webserver:/var/lib/webserver:Z iss2023/lima-web-stripped_i
+docker run -d --net iss2023/lima_n --ip 198.51.100.149 --hostname www.iss.cyber23.test --add-host db.iss.cyber23.test:198.51.100.150 -p 8080:80 --name iss2023-lima-web_c --security-opt seccomp=$PWD/docker_webserver.json --security-opt label:type:docker_webserver_c_t iss2023/lima-web-stripped_i
